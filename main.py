8ecdee375b351bb3b39633657cc2d892d8f1dddf
yujiro-miyabayashi <yujiro-miyabayashi@phiaro.jp>
2024-02-27 12:34:12 +0900

    add test app

---
+++ main.py
@@ -0,0 +1,56 @@
+import time
+from whill import ComWHILL
+from pythonosc.udp_client import SimpleUDPClient
+
+COM_PORT = 'COM40'
+OSC_SERVER = '127.0.0.1'
+OSC_PORT = 12345
+
+
+def callback0():
+    global whill 
+    whill.start_data_stream(20, 1, 5)
+
+def callback1():
+    global whill 
+    whill.start_data_stream(20, 0, 5)
+
+def main():
+
+    whill = ComWHILL(port=COM_PORT)
+    client = SimpleUDPClient(OSC_SERVER, OSC_PORT)
+
+    whill.register_callback('data_set_0', callback0)
+    whill.register_callback('data_set_1', callback1)
+    whill.start_data_stream(20, 1, 5)
+
+    # power on
+    whill.send_power_on()
+
+    # safe mode
+    whill.set_safe_mode(True)
+
+    while True:
+       
+        # disable joystick
+        whill.send_joystick(front=0, side=0) 
+
+        whill.refresh()
+        time.sleep(0.05)
+
+        level, current = whill.battery.values()
+        print('joystick: {fb} {lr} gear:{gear} Battery: {level}%, {current}mA'.format(
+            fb=whill.joy["front"], 
+            lr=whill.joy["side"], 
+            gear = whill.speed_mode_indicator+1,
+            level=level, current=current))
+        
+        # send osc data
+        _fb = whill.joy["front"]
+        _lr = whill.joy["side"]
+        _gear = whill.speed_mode_indicator+1
+        client.send_message("/whill/data", [_fb, _lr, _gear])
+
+
+if __name__ == "__main__":
+    main()
